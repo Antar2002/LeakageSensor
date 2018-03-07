8ec87939dc54bd2709463f87fed6b499a3c82f5c
Antar2002 <antar2002@mail.ru>
2018-03-07 04:31:53 +0300

    1-wire slave firmware added.

---
+++ Firmware_Slave/LeakageSensor_Slave.c
@@ -0,0 +1,359 @@
+#include <avr/eeprom.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <util/delay.h>
+#include "LeakageSensor_Slave.h"
+#include "D:\_Projects\MK\LeakageSensor\Firmware\wire1\wire1_lite.h"
+
+uint8_t mcusr_mirror __attribute__ ((section (".noinit")));
+
+
+void get_mcusr(void) \
+  __attribute__((naked)) \
+  __attribute__((section(".init3")));
+void get_mcusr(void)
+{
+	mcusr_mirror = MCUSR;
+	MCUSR = 0;
+	wdt_disable();
+}
+
+
+ISR(TIMER0_OVF_vect)
+{
+
+	// Отсчет времени, в течение которого считается кол-во импульсов с датчика
+	impulsTimerCnt--;
+	if(impulsTimerCnt==0){
+		//curFrequency = freqCnt;
+		//freqCnt = 0;
+		curFrequency = TCNT1;
+		TCNT1 = 0;
+		impulsTimerCnt = IMPULS_MES_TIME;
+		freqUpdated = 1;
+	}
+
+	needHandleTimer0 = 1;
+
+	TCNT0 = TIMER;
+}
+
+void handleTimer0(){
+
+	// Подавление дребезга кнопки
+	if(btnNoiseTimer>0){
+		btnNoiseTimer--;
+		if(btnNoiseTimer==0){
+			btnPressed = 1;
+			btnPressTimer = 0;
+		}
+	}
+
+	// Обработка мигания диодом
+	if(led_cnt<=0){
+		if(led_cycle<=0){
+			LED_PORT &= ~_BV(LED_PIN);
+		}
+		else{
+			LED_PORT ^= _BV(LED_PIN);
+			// Если кол-во циклов ENDLESS_LED_CYCLE и более - бесконечное мигание, пока кто-то не обнулит led_cycle
+			if(led_cycle < ENDLESS_LED_CYCLE)
+				led_cycle--;
+			led_cnt = led_cnt_max;
+		}
+	}
+	else{
+		// Считать время свечения диода только, если счетчик меньше 60 000. Иначе его можно включать и вылкючать извне
+		if(led_cnt < ENDLESS_LED_PULSE)
+			led_cnt--;
+	}
+
+}
+
+// Обработка кнопки
+ISR(INT0_vect)
+{
+	// Кнопку нажали
+	if(~BUTTON_PIN & _BV(BUTTON)){
+		if(btnNoiseTimer==0 && !btnPressed){
+			btnNoiseTimer = BTN_NOIS_REDUCE;
+		}
+	}
+	// Кнопку отпустили
+	else{
+		// Если кнопка сочтена нажатой и была отпущена - обработать время нажатия
+		if(btnPressed && !pressedForResetAlarm){
+			modeChanged = 1;
+			mode = 1;
+		}
+		btnNoiseTimer = 0;
+		btnPressed = 0;
+		pressedForResetAlarm = 0;
+	}
+}
+
+// Прерывание по 1-wire
+ISR(PCINT2_vect)
+{
+	char cmd = 0xFF;
+
+
+	if(WIRE1_PIN & _BV(WIRE1))
+		return;
+
+	// Выключить это прерывание
+	PCICR &= ~_BV(PCIE2);
+
+	cmd = wire1_listener();
+
+	// If this is "set default" command (check with reversed value)
+	if(cmd==0b10100110){
+		sendByteSlave(~wire1_address);
+		mode = 1;
+		modeChanged = 1;
+	}
+	// If this is "check alarm state" command (check with reversed value)
+	if(cmd==0b00000000 && alarmCnt>ALARM_MIN_CNT){
+		sendByteSlave(~wire1_address);
+	}
+
+	// Включить это прерывание
+	PCICR |= _BV(PCIE2);
+
+}
+
+char measureFrequency(){
+
+	uint16_t tmpFrequency = 0;
+
+	// Если это первый запуск - инициализировать счетчик замеров и т.п.
+	if(measureCnt==0){
+		measureCnt = MES_COUNT;
+		cli();
+		tmpFrequency = curFrequency;
+		freqUpdated = 0;
+		sei();
+	}
+
+	// Если получен очередной замер частоты
+	if(freqUpdated){
+		cli();
+		// Усреднять, только если это не первый замер
+		if(measureCnt < MES_COUNT)
+			tmpFrequency = (tmpFrequency>>1) + (curFrequency>>1);
+		freqUpdated = 0;
+		measureCnt--;
+		if(measureCnt==0){
+			lastFrequency = tmpFrequency;
+			return 0;
+		}
+		sei();		
+	}
+	return 1;
+}
+
+void portsInit()
+{
+	// Init sensor input
+	SENSOR_DDR &= ~_BV(SENSOR);
+	SENSOR_PORT |= _BV(SENSOR);
+
+	// wire-1 address
+	WIRE1_ADDR_DDR &= ~(_BV(WIRE1_ADDR_1) | _BV(WIRE1_ADDR_2));
+	WIRE1_ADDR_PORT |= _BV(WIRE1_ADDR_1) | _BV(WIRE1_ADDR_2);
+
+	// Initialize LED (PC5)
+	LED_DDR |= _BV(LED_PIN);
+	LED_PORT &= ~_BV(LED_PIN);
+
+	// Initialize button (PD2)
+	BUTTON_DDR &= ~(_BV(BUTTON));
+	BUTTON_PORT |= _BV(BUTTON);
+}
+
+
+void timersInit()
+{
+	//Init Timer 0
+	initTimer0ForSignal();
+
+	// Init timer 1 (for sensor frequency measuring)
+	TCCR1A = 0x00;
+	TCCR1B = (1<<CS12) | (1<<CS11) | (1<<CS10);
+	TCNT1=0x00;
+	TIMSK1 = 0x00;
+
+
+}
+
+void initTimer0ForSignal(){
+	TCCR0B = (1<<CS02);		// /256 (39,0625 kHz / 256 = 152,588)
+	TCNT0 = TIMER;
+	TIMSK0 = (1<<TOIE0); // Включить прерывание по переполнению таймера
+}
+
+void initTimer0ForWire1(){
+	TCCR0B = _BV(CS01);		// /8 (1250 kHz / 256 = 4882,8125)
+	TCNT0 = 0;
+	TIMSK0 = 0; // Выключить прерывание по переполнению таймера
+}
+
+void wire1_addr_init(){
+
+	char offset;
+
+	offset = (WIRE1_ADDR_PIN & _BV(WIRE1_ADDR_1))>>WIRE1_ADDR_1;
+	offset |=  (WIRE1_ADDR_PIN & _BV(WIRE1_ADDR_2))>>WIRE1_ADDR_2-1;
+
+	wire1_address = (1<<offset);
+//wire1_address = 0b00100000;
+
+}
+
+
+int main(void)
+{
+
+	DDRC = 0x00;
+	PORTC = 0xFF;
+
+	portsInit();
+
+	cli();
+
+	// Init INT0
+	EICRA = 0x00;
+	EICRA |= _BV(ISC00);
+	EIMSK = 0x00;
+	EIMSK |= _BV(INT0);
+
+	// INT of 1-wire
+	PCMSK2 |= _BV(PCINT23);
+	PCICR |= _BV(PCIE2);
+
+	// Disable TWI
+	TWCR &= ~_BV(TWEN);
+
+	timersInit();
+
+	// Disable Analog Comparator
+	ACSR &= ~_BV(ACIE);
+	ACSR |= _BV(ACD);
+
+	// Не делать так - отключает цифровые входы нафиг
+	/*DIDR1 = 0xFF;
+	DIDR0 = 0xFF;*/
+
+	wire1_init();
+	wire1_addr_init();
+
+
+	SPCR &= ~_BV(SPE);
+
+	// Load default frequency from EEPROM
+	defaultFrequency = eeprom_read_word(&default_frequency_stored);
+
+	if(defaultFrequency == 65535)
+		mode = 1;
+	else
+		mode = 2;
+	modeChanged = 1;
+
+
+	sei();
+
+
+
+	treshold = TRESHOLD;
+
+DDRB |= _BV(PB0);
+PORTB &= ~_BV(PB0);
+
+
+
+	while(1)
+	{
+		wdt_reset();
+
+		if(needHandleTimer0){
+			// Обработать то, что должно было быть сделано прерыванием TIMER0
+			cli();
+			needHandleTimer0 = 0;
+			handleTimer0();
+			sei();
+		}
+
+		// Основные режимы работы
+		switch(mode){
+			case 1:		// Замер фоновой частоты
+				if(modeChanged){
+					startLed(ENDLESS_LED_CYCLE, ENDLESS_LED_PULSE);
+					modeChanged = 0;
+				}
+				if(!measureFrequency()){
+					// Если измеренное значение похоже на правду
+					if(lastFrequency>100){
+						defaultFrequency = lastFrequency;
+						// Вычислить и запомнить порог срабатывания, при превышении которого нужно поднять тревогу
+						unsigned long tmp = defaultFrequency;
+						tmp *= (100 - treshold);
+						tmp = tmp / 100; // Вроде работает, когда вот так растянуто по строчкам
+						//unsigned long tmp = defaultFrequency * (1-0.05); // Работает
+						defaultFrequency = tmp;
+
+						// Save this value to EEPROM
+						eeprom_write_word (&default_frequency_stored, defaultFrequency);
+
+						led_cycle = 0;
+
+						// Перейти в режим мониторинга
+						mode = 2;
+						modeChanged = 1;
+					}
+				}
+				break;
+			case 2:		// Контроль протечки
+				if(modeChanged){
+					modeChanged = 0;
+				}
+				if(!measureFrequency()){
+					if(lastFrequency < defaultFrequency){
+startLed(ENDLESS_LED_CYCLE, ENDLESS_LED_PULSE);
+						if(alarmCnt<ALARM_MIN_CNT*2){
+							alarmCnt++;
+						}
+						if(alarmCnt>ALARM_MIN_CNT){
+//							if(led_cnt_max!=LED_ALARM_PULSE)
+//								startLed(ENDLESS_LED_CYCLE, LED_ALARM_PULSE);
+						}
+					}
+					else{
+						if(alarmCnt!=0){
+							cli();
+							alarmCnt--;
+							sei();
+						}
+						else{
+							// Выключить светодиод
+							led_cycle = 0;
+						}
+led_cycle = 0;
+					}
+				}
+				break;
+		}
+	}
+
+}
+
+void startLed(int cycles, int ledCnt)
+{
+	LED_PORT |= _BV(LED_PIN);
+	led_cycle = (cycles * 2) - 1;
+	led_cnt_max = ledCnt;
+	led_cnt = led_cnt_max;
+}
---
+++ Firmware_Slave/LeakageSensor_Slave.h
@@ -0,0 +1,85 @@
+#if defined(__AVR_ATmega88__) \
+|| defined(__AVR_ATmega88A__) \
+|| defined(__AVR_ATmega88P__) \
+|| defined(__AVR_ATmega88PA__)
+#define		SENSOR_DDR		DDRD
+#define		SENSOR_PORT		PORTD
+#define		SENSOR_PIN		PIND
+#define		SENSOR			PD5				// Must be TIMER1 input (T1)
+//#define		SENSOR			PB0			// Must be ICP1 (for ATMEGA88 it's PB0) - NOT ACTUAL
+
+#define		WIRE1_ADDR_DDR		DDRC
+#define		WIRE1_ADDR_PORT		PORTC
+#define		WIRE1_ADDR_PIN		PINC
+#define		WIRE1_ADDR_1		PC0
+#define		WIRE1_ADDR_2		PC1
+
+#define		LED_DDR			DDRC
+#define		LED_PORT		PORTC
+#define		LED_PIN			PC3
+
+#define		BUTTON_DDR		DDRD
+#define		BUTTON_PORT		PORTD
+#define		BUTTON_PIN		PIND
+#define		BUTTON			PD2			// Must be INTO input
+#endif
+
+#if defined(__AVR_ATtiny24A__) \
+|| defined(__AVR_ATtiny48__)
+#define		SENSOR_DDR		DDRA
+#define		SENSOR_PORT		PORTA
+#define		SENSOR_PIN		PINA
+#define		SENSOR			PA4				// Must be TIMER1 input (T1)
+//#define		SENSOR			PB0			// Must be ICP1 (for ATMEGA88 it's PB0) - NOT ACTUAL
+
+#define		WIRE1_ADDR_DDR		DDRA
+#define		WIRE1_ADDR_PORT		PORTA
+#define		WIRE1_ADDR_PIN		PINA
+#define		WIRE1_ADDR_1		PA0
+#define		WIRE1_ADDR_2		PA1
+#endif
+
+
+#define		IMPULS_MES_TIME	100			// Кол-во переполнений таймера, в течение которого буду считаться импульсы
+#define		MES_COUNT		10			// Кол-во замеров частоты для усреднения
+//#define		TRESHOLD		0.05
+#define		ALARM_MIN_CNT	10			// Кол-во превышений, после которого объявляется тревога (около 6 сек)
+
+#define		TIMER			250			// Период переполнения Timer0 (256-6; OVF = 6510,4Hz)
+
+#define		TRESHOLD		5			// Процент изменения фоновой частоты, обозначающий тревогу
+
+#define		ENDLESS_LED_CYCLE	255		// Значение для бесконечного мигания светодиодом
+#define		ENDLESS_LED_PULSE	60000	// Значение для бесконечного свечения светодиода
+#define		LED_ALARM_PULSE	3000			// Период мигания светодиода при тревоге (в переполнениях Timer0)
+
+#define		BTN_NOIS_REDUCE	45			// ~0.05 сек
+
+EEMEM	uint16_t 	default_frequency_stored = 65535;
+
+static volatile char mode;
+static volatile char modeChanged = 0;		// Флаг смены режима. Как только кто-то меняет режим - взводится до начала обработки режима. 
+
+static char impulsTimerCnt = IMPULS_MES_TIME;
+static volatile uint16_t curFrequency = 0;		// Текущая частота. Заполняется в прерывании.
+
+static volatile char measureCnt = 0;		// Счетчик замеров частоты. При измерении частоты текущее значение (curFrequency) усредняется пока тикает этот счетчик.
+static volatile char freqUpdated = 0;		// Флаг/признак, что пришло новое измерение частоты
+
+uint16_t lastFrequency = 0;
+uint16_t defaultFrequency = 0;
+
+volatile char alarmCnt = 0;			// Счетчик превышений порога фоновой частоты
+
+char treshold;				// Разница между нормой и текущим уровнем, при которой будет тревога
+
+volatile char needHandleTimer0 = 0;
+
+volatile int btnNoiseTimer = 0;
+volatile char btnPressed = 0;
+volatile int btnPressTimer = 0;
+volatile char pressedForResetAlarm = 0; // Флаг/признак того, что кнопку нажимали, чтобы сбросить тревогу
+
+volatile int led_cnt = 0;
+volatile int led_cycle = 0;
+volatile int led_cnt_max = 0;
